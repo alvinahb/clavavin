a81606cf5931a6ab8cbb0c4caa86f8637e2ec615
Alvin Ahb <60699542+alvinahb@users.noreply.github.com>
2023-04-13 14:56:21 +0200

    Initial app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# clavavin
+# Clavavin
+
+This is the repository for Clavavin application.
+
+- Language : french
+- Built in Go version 1.20
+- Uses the [chi](github.com/go-chi/chi/v5) router
+- Uses [alex edwards SCS](github.com/alexedwards/scs/v2) session management
+- Uses [nosurf](github.com/justinas/nosurf)
---
+++ pkg/render/render.go
@@ -0,0 +1,89 @@
+package render
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/alvinahb/clavavin/pkg/config"
+	"github.com/alvinahb/clavavin/pkg/models"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefalutData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate renders templates using html template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// Get the template cache from the AppConfig
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	// Get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefalutData(td)
+
+	_ = t.Execute(buf, td)
+
+	// Render the template
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println("Error writing template to browser", err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	// Get all of the files named *.page.tmpl from ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	// Range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
