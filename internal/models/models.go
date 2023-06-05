3e14c6f10d0b5d54879a71d92e5ed77c843f57cd
Alvin Ahb <60699542+alvinahb@users.noreply.github.com>
2023-06-05 15:09:04 +0200

    Initialize models for database connection

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,42 @@
 package models
 
+import "time"
+
+// User is the user model
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Wine is the wine model
 type Wine struct {
-	Bottle string
-	Domain string
-	Year string
-	Location string
-	Color string
+	ID              int
+	Name            string
+	Domain          string
+	Year            string
+	AppellationType string
+	AppellationName string
+	Location        string
+	Color           string
+	Culture         string
+	Varieties       []string
+	Robe            string
+	Nose            string
+	Taste           string
+	Dishes          []string
+	Season          string
+}
+
+// Opinion is the opinion model
+type Opinion struct {
+	ID      int
+	Wine    Wine
+	Rate    int
+	Comment string
 }
