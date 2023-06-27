0d6d4271792db393c52e7ab2937a2df9f52cffa8
Alvin Ahb <60699542+alvinahb@users.noreply.github.com>
2023-06-27 16:34:32 +0200

    Initialize wines migrations seed

---
+++ migrations/20230627142812_seed_wines_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.wines (name,"domain","year",appellation_name,"location",color,culture,robe,nose,taste,dishes,season,created_at,updated_at,varieties,appellation_type) VALUES
+	 ('Bouteille de Test','Domaine de Test','2020','Appellation de Test','Languedoc','Orange','','','','','','','2023-06-09 12:49:55.900','2023-06-09 12:49:55.900','','AOP'),
+	 ('Bouteille de Claire','Domaine de Claire','1996','Label Claire','ClaireLand','Orange','','','','','','','2023-06-09 16:08:42.862','2023-06-09 16:08:42.862','','AOC');
